570119c2db30f122c40331973d4e9c38c1c623a5
fRenzy <fRenzyA05@gmail.com>
2022-10-18 14:52:14 -0300

    Updated code and added level system
    May need to remove UI_Info and simplify it back

--- APIHealthSystemProject/Program.cs
+++ APIHealthSystemProject/Program.cs
@@ -16,66 +16,151 @@ namespace APIHealthSystemProject
         static void Main(string[] args)
         {
             health = 100;
-            shield = 50;
+            shield = 100;
             lives = 3;
             experience = 0;
             level = 1;
 
-            TakeDamage(0);
-            Heal(30);
-            RegenShield(-40);
+            Heal(100);
+            RegenShield(10);
+            TakeDamage(600);
+            LevelUp(400);
+            Console.WriteLine();
+            UI_Info();
             ShowHUD();
 
-            RESET();
-            ShowHUD();
+           
             Console.ReadKey(true);
         }
 
         static void ShowHUD()
         {
-            
-            if (health > 100 && shield > 50)
+            if (health > 100)
             {
                 Console.WriteLine();
-                Console.WriteLine("|OVER| Health: " + health);
-                Console.WriteLine("|FULL| Shield: " + shield);
-                Console.WriteLine("Level: " + level + "|EXP: " + experience);
-                Console.WriteLine("Lives: " + lives);
-                Console.WriteLine();
-            }
-            else if (health > 100)
-            {
-                Console.WriteLine();
+                Console.WriteLine("==============================");
                 Console.WriteLine("|OVER| Health: " + health);
                 Console.WriteLine("Shield: " + shield);
                 Console.WriteLine("Level: " + level + "|EXP: " + experience);
                 Console.WriteLine("Lives: " + lives);
+                Console.WriteLine("==============================");
                 Console.WriteLine();
             }
-
-            else if (shield > 50)
+            else if (health <= 75 && health >= 25)
             {
                 Console.WriteLine();
-                Console.WriteLine("Health: " + health);
-                Console.WriteLine("|FULL| Shield: " + shield);
+                Console.WriteLine("==============================");
+                Console.WriteLine("Health: " + health + " |HURT|");
+                Console.WriteLine("Shield: " + shield);
                 Console.WriteLine("Level: " + level + "|EXP: " + experience);
                 Console.WriteLine("Lives: " + lives);
+                Console.WriteLine("==============================");
+                Console.WriteLine();
+            }
+            else if (health <= 25)
+            {
+                Console.WriteLine();
+                Console.WriteLine("==============================");
+                Console.WriteLine("Health: " + health + " |DANGER|");
+                Console.WriteLine("Shield: " + shield);
+                Console.WriteLine("Level: " + level + "|EXP: " + experience);
+                Console.WriteLine("Lives: " + lives);
+                Console.WriteLine("==============================");
                 Console.WriteLine();
             }
 
             else
             {
                 Console.WriteLine();
+                Console.WriteLine("==============================");
                 Console.WriteLine("Health: " + health);
                 Console.WriteLine("Shield: " + shield);
                 Console.WriteLine("Level: " + level + "|EXP: " + experience);
                 Console.WriteLine("Lives: " + lives);
+                Console.WriteLine("==============================");
                 Console.WriteLine();
             }
         }
 
+
+        static void RESET()
+        {
+            Console.WriteLine("All stats reset");
+            health = 100;
+            shield = 100;
+            lives = 3;
+        }
+        static void Heal(int healup)
+        {
+            int currenthealth = health;
+            if (healup > 0)
+            {
+                Console.WriteLine("DEBUG: About to heal for " + healup + "HP");
+            }
+            currenthealth = currenthealth + healup;
+            if (currenthealth > 150)
+            {
+                health = 150;
+            }
+            else if (healup < 0)
+            {
+                Console.WriteLine("About to heal for " + healup + " HP");
+                Console.WriteLine("ERROR: Cannot have a negative health integer value");
+            }
+            else
+            {
+                health = currenthealth;
+            }
+        }
+
+        static void RegenShield(int shieldrepair)
+        {
+            int currentshield = shield;
+            if (shieldrepair > 0)
+            {
+                Console.WriteLine("DEBUG: About to repair " + shieldrepair + " SHIELD");
+            }
+            currentshield = currentshield + shieldrepair;
+            if (currentshield > 100)
+            {
+                shield = 100;
+            }
+            if (shieldrepair < 0)
+            {
+                Console.WriteLine("About to repair " + shieldrepair + "SHIELD");
+                Console.WriteLine("ERROR: Cannot have a negative shield integer value");
+            }
+            else
+            {
+                shield = currentshield;
+            }
+        }
+        static void LevelUp(int currentEXP)
+        {
+            int expCap = 100;
+            if (currentEXP > 0)
+            {
+                Console.WriteLine("DEBUG: About to gain " + currentEXP + " points of experience");
+            }
+          
+            Console.WriteLine();
+            
+            while (currentEXP >= expCap)
+            {
+                level = level + 1;
+                currentEXP = currentEXP - expCap;
+                expCap = expCap + expCap;
+                
+            }
+            experience = currentEXP;
+            
+        }
         static void TakeDamage(int damage)
         {
+            if (damage > 0)
+            {
+                Console.WriteLine("DEBUG: About to take " + damage + "damage");
+            }
             if (shield < damage)
             {
                 health = (health + shield) - damage;
@@ -91,8 +176,10 @@ namespace APIHealthSystemProject
             }
             if (health <= 0)
             {
-                lives = lives - 1;
                 Console.WriteLine("Player took too much damage. Player loses a life");
+                lives = lives - 1;
+                health = 100;
+                shield = 100;
             }
             if (lives == 0)
             {
@@ -100,54 +187,24 @@ namespace APIHealthSystemProject
             }
             
         }
-
-        static void RESET()
+        static void UI_Info()
         {
-            health = 100;
-            shield = 50;
-            lives = 3;
-        }
-        static void Heal(int healup)
-        {
-            int currenthealth = health;
-            currenthealth = currenthealth + healup;
-            if (healup < 0)
+            if (health > 150)
             {
-                Console.WriteLine("ERROR: Cannot have a negative health integer value");
-            }
-            else if (currenthealth >= 150)
-            {
-                health = 150;
                 Console.WriteLine("Max |OVERHEAL|");
             }
-            else
+            if (shield > 100)
             {
-                health = currenthealth;
-            }
-        }
-
-        static void RegenShield(int shieldrepair)
-        {
-            int currentshield = shield;
-            currentshield = currentshield + shieldrepair;
-            if (shieldrepair < 0)
-            {
-                Console.WriteLine("ERROR: Cannot have a negative shield integer value");
-            }
-            else if (currentshield > 50)
-            {
-                shield = 50;
                 Console.WriteLine("|FULL| Shield");
             }
-            else
+            if (experience > 0)
             {
-                shield = currentshield;
+                Console.WriteLine("You have " + experience + " EXP");
             }
-        }
-
-        static void LevelUp()
-        {
-
+            if (level + 1 > level)
+            {
+                Console.WriteLine("You LEVELED up");
+            }
         }
     }
 }
